b1a7526a36d03c1c4b51de68f55c025d1a721bb0
Siwen Yu <yusiwen@gmail.com>
2019-08-19 14:15:01 +0800

    Rename spider

--- app_monitor/spiders/github.py
+++ app_monitor/spiders/github.py
@@ -4,21 +4,25 @@ import json
 
 from app_monitor.items import AppMonitorItem
 
-class GiteaSpider(scrapy.Spider):
-    name = 'gitea'
-    allowed_domains = ['api.github.com/repos/go-gitea/gitea/releases']
-    start_urls = ['http://api.github.com/repos/go-gitea/gitea/releases']
+class GithubSpider(scrapy.Spider):
+    name = 'github'
+    allowed_domains = ['api.github.com']
+    start_urls = [
+        'http://api.github.com/repos/go-gitea/gitea/releases',
+        'https://api.github.com/repos/git-for-windows/git/releases'
+    ]
 
     def parse(self, response):
         json_dict = json.loads(response.body_as_unicode())
         filtered_dict = json_dict[0]
         version = filtered_dict['tag_name']
         date = filtered_dict['created_at']
+        name = response.url.rsplit('/', 2)[-2]
 
         item = AppMonitorItem()
-        item['name'] = 'Gitea'
+        item['name'] = name
         item['version'] = version
         item['date'] = date
         item['notes'] = ''
-        item['id'] = 'gitea'
+        item['id'] = name
         return item
