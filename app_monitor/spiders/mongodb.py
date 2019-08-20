0b0022a672587c8feda8cbd018eef6452170fda2
Siwen Yu <yusiwen@gmail.com>
2019-08-20 20:37:21 +0800

    Add mongodb spider

---
+++ app_monitor/spiders/mongodb.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+import scrapy
+
+from app_monitor.items import AppMonitorItem
+
+class MongodbSpider(scrapy.Spider):
+    name = 'mongodb'
+    allowed_domains = ['www.mongodb.com']
+    start_urls = ['https://www.mongodb.com/download-center/community']
+
+    def parse(self, response):
+        tmp = response.xpath('//main//select/option[text()[re:test(.,".*current\srelease\)$")]]/text()').get()
+        version = tmp.split()[0]
+
+        item = AppMonitorItem()
+        item['name'] = 'MongoDB current release'
+        item['version'] = version
+
+        item['date'] = ''
+
+        item['notes'] = ''
+        item['id'] = 'mongodb'
+        return item
