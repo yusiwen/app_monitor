2d8af367d862c8535799e3cac99676fe3bd06423
Siwen Yu <yusiwen@gmail.com>
2019-08-23 07:42:02 +0800

    Add changelog of chrome

--- app_monitor/spiders/chrome.py
+++ app_monitor/spiders/chrome.py
@@ -12,20 +12,17 @@ class ChromeSpider(scrapy.Spider):
     def parse(self, response):
         json_dict = json.loads(response.body_as_unicode())
         filtered_dict = [x for x in json_dict if x['os']=='win64']
-        print(json.dumps(filtered_dict))
         versions = filtered_dict[0]['versions']
         output = [x for x in versions if x['channel']=='stable']
-        print(json.dumps(output))
         version = '{current_version}'.format(**output[0])
-        print(version)
         date = '{current_reldate}'.format(**output[0])
-        print(date)
+        previous_version = '{previous_version}'.format(**output[0])
 
         item = AppMonitorItem()
         item['name'] = 'google-chrome-browser'
         item['version'] = version
         item['date'] = date
-        item['notes'] = ''
+        item['notes'] = '<a href="https://chromium.googlesource.com/chromium/src/+log/{}..{}?pretty=fuller&n=10000">Changelog</a>'.format(previous_version, version)
         item['id'] = 'chrome'
         item['download_url'] = 'https://www.google.com/intl/en/chrome/browser/desktop/index.html?standalone=1'
         return item
