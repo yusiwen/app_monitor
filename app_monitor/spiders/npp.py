7cd8341ce707284ef8218b6c4262796d80aa4659
Siwen Yu <yusiwen@gmail.com>
2019-08-22 13:31:19 +0800

    Upate npp spider

--- app_monitor/spiders/npp.py
+++ app_monitor/spiders/npp.py
@@ -20,7 +20,10 @@ class NppSpider(scrapy.Spider):
         date = tmp.split(':')[1]
         item['date'] = date.strip()
 
-        notes = response.xpath('//div[@id="main"]/div/ol/li/text()').extract()
+        notes = ''
+        tmp = response.xpath('//div[@id="main"]/div/ol').extract()
+        notes = notes.join(tmp)
+
         item['notes'] = notes
         item['id'] = 'npp'
         item['download_url'] = 'https://notepad-plus-plus.org' + response.xpath('//div[@id="main"]//a[text()[re:test(., "^Notepad.*zip.*x64$")]]/@href').get()
