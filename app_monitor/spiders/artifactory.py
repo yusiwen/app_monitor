7bc649f31ffd527d0b75dd22cd7e999c9abaad73
Siwen Yu <yusiwen@gmail.com>
2020-10-12 14:41:22 +0800

    format code

--- app_monitor/spiders/artifactory.py
+++ app_monitor/spiders/artifactory.py
@@ -3,15 +3,17 @@ import scrapy
 
 from app_monitor.items import AppMonitorItem
 
+
 class ArtifactorySpider(scrapy.Spider):
     name = 'artifactory'
     allowed_domains = ['bintray.com/jfrog']
-    start_urls = ['https://bintray.com/jfrog/artifactory-debs/jfrog-artifactory-oss/_latestVersion']
+    start_urls = [
+        'https://bintray.com/jfrog/artifactory-debs/jfrog-artifactory-oss/_latestVersion']
 
     def parse(self, response):
         version = response.url.rsplit('/', 1)[-1]
         date = response.xpath('//div[@id="versionPublishedDate"]/text()').get()
-        date = date.split('Updated in',1)[1].strip()
+        date = date.split('Updated in', 1)[1].strip()
 
         item = AppMonitorItem()
         item['name'] = 'jfrog-artifactory-oss'
@@ -19,5 +21,6 @@ class ArtifactorySpider(scrapy.Spider):
         item['date'] = date
         item['notes'] = ''
         item['id'] = 'artifactory'
-        item['download_url'] = 'https://bintray.com' + response.xpath('//div[@id="main-content"]//div[@class="nodeDetails"]/a/@href').get()
+        item['download_url'] = 'https://bintray.com' + response.xpath(
+            '//div[@id="main-content"]//div[@class="nodeDetails"]/a/@href').get()
         return item
