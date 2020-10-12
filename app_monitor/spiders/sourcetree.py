6dcb5cacd11289b21647fc14e94ba3e5aee26844
Siwen Yu <yusiwen@gmail.com>
2020-10-12 15:38:07 +0800

    fix sourcetree crawler

--- app_monitor/spiders/sourcetree.py
+++ app_monitor/spiders/sourcetree.py
@@ -3,38 +3,32 @@ import scrapy
 
 from app_monitor.items import AppMonitorItem
 
+
 class SourcetreeSpider(scrapy.Spider):
     name = 'sourcetree'
-    allowed_domains = ['www.sourcetreeapp.com', 'product-downloads.atlassian.com']
-    start_urls = ['https://www.sourcetreeapp.com/download-archives']
+    allowed_domains = ['www.sourcetreeapp.com',
+                       'product-downloads.atlassian.com']
+    start_urls = ['https://www.sourcetreeapp.com']
 
-    def _parse_item(self, id, tag, response):
-        dwn_url_mac = response.xpath('//div[@id="download-area"]//h3[text()[re:test(.,"^' + tag + '$")]]/../following-sibling::div[1]//tr[1]/td[1]/div/a/@href').get()
-        version_mac = response.xpath('//div[@id="download-area"]//h3[text()[re:test(.,"^' + tag + '$")]]/../following-sibling::div[1]//tr[1]/td[1]/div/a/b/text()').get()
-        date_mac = response.xpath('//div[@id="download-area"]//h3[text()[re:test(.,"^' + tag + '$")]]/../following-sibling::div[1]//tr[1]/td[3]/text()').get()
-        release_note_url = response.xpath('//div[@id="download-area"]//h3[text()[re:test(.,"^' + tag + '$")]]/../following-sibling::div[1]//tr[1]/td[1]/small/a/@href').get()
-
+    def _parse_item(self, app_id, tag, response):
+        if tag == 'Windows':
+            dwn_url = response.xpath(
+                '//div[@id="download-area"]//a[contains(text(), "Windows")]/@href').get()
+            version = dwn_url.rsplit(
+                '/')[-1].replace('.exe', '').replace('SourceTreeSetup-', '')
+        else:
+            dwn_url = response.xpath(
+                '//div[@id="download-area"]//a[contains(text(), "Mac")]/@href').get()
+            version = dwn_url.rsplit(
+                '/')[-1].replace('.zip', '').replace('Sourcetree_', '')
 
         item = AppMonitorItem()
         item['name'] = 'SourceTree ' + tag
-        item['version'] = version_mac
-
-        item['date'] = date_mac
-
+        item['version'] = version
+        item['date'] = ''
         item['notes'] = ''
-        item['id'] = id
-        item['download_url'] = dwn_url_mac
-        yield scrapy.Request(release_note_url, meta={'item':item}, callback=self._parse_release_notes)
-
-    def _parse_release_notes(self, response):
-        item = response.meta['item']
-        if 'windows' in response.url:
-            item['notes'] = response.xpath('//body').get()
-        else:
-            tmp = ''
-            tmp = tmp.join(response.xpath('//a[1]/following-sibling::li').extract())
-            item['notes'] = tmp
-
+        item['id'] = app_id
+        item['download_url'] = dwn_url
         yield item
 
     def parse(self, response):
