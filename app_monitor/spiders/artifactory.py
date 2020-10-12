7437453136c9f8d36b9471a200563222423a340d
Siwen Yu <yusiwen@gmail.com>
2020-10-12 13:04:47 +0800

    update artifactory crawler for latest oss page

--- app_monitor/spiders/artifactory.py
+++ app_monitor/spiders/artifactory.py
@@ -6,15 +6,15 @@ from app_monitor.items import AppMonitorItem
 class ArtifactorySpider(scrapy.Spider):
     name = 'artifactory'
     allowed_domains = ['bintray.com/jfrog']
-    start_urls = ['https://bintray.com/jfrog/artifactory-debs/jfrog-artifactory-oss-deb/_latestVersion']
+    start_urls = ['https://bintray.com/jfrog/artifactory-debs/jfrog-artifactory-oss/_latestVersion']
 
     def parse(self, response):
         version = response.url.rsplit('/', 1)[-1]
         date = response.xpath('//div[@id="versionPublishedDate"]/text()').get()
-        date = date.split('Updated',1)[1].strip()
+        date = date.split('Updated in',1)[1].strip()
 
         item = AppMonitorItem()
-        item['name'] = 'jfrog-artifactory-oss-deb'
+        item['name'] = 'jfrog-artifactory-oss'
         item['version'] = version
         item['date'] = date
         item['notes'] = ''
