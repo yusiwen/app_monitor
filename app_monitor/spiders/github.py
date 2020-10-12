8798ad8a72278f3971e3eb178672c3509a303079
Siwen Yu <yusiwen@gmail.com>
2020-10-12 16:13:58 +0800

    fix github crawler

--- app_monitor/spiders/github.py
+++ app_monitor/spiders/github.py
@@ -9,14 +9,20 @@ class GithubSpider(scrapy.Spider):
     name = 'github'
     allowed_domains = ['api.github.com']
     start_urls = [
-        'https://api.github.com/repos/go-gitea/gitea/releases',
-        'https://api.github.com/repos/git-for-windows/git/releases',
-        'https://api.github.com/repos/keeweb/keeweb/releases',
-        'https://api.github.com/repos/zeit/hyper/releases',
-        'https://api.github.com/repos/gitextensions/gitextensions/releases',
-        'https://api.github.com/repos/cmderdev/cmder/releases',
-        'https://api.github.com/repos/shadowsocks/ShadowsocksX-NG/releases',
-        'https://api.github.com/repos/be5invis/Sarasa-Gothic/releases',
+        'https://api.github.com/repos/go-gitea/gitea/releases/latest',
+        'https://api.github.com/repos/git-for-windows/git/releases/latest',
+        'https://api.github.com/repos/keeweb/keeweb/releases/latest',
+        'https://api.github.com/repos/gitextensions/gitextensions/releases/latest',
+        'https://api.github.com/repos/cmderdev/cmder/releases/latest',
+        'https://api.github.com/repos/shadowsocks/shadowsocks-windows/releases/latest',
+        'https://api.github.com/repos/shadowsocks/ShadowsocksX-NG/releases/latest',
+        'https://api.github.com/repos/Dreamacro/clash/releases/latest',
+        'https://api.github.com/repos/Fndroid/clash_for_windows_pkg/releases/latest',
+        'https://api.github.com/repos/be5invis/Sarasa-Gothic/releases/latest',
+        'https://api.github.com/repos/Eugeny/terminus/releases/latest',
+        'https://api.github.com/repos/dbeaver/dbeaver/releases/latest',
+        'https://api.github.com/repos/microsoft/vscode/releases/latest',
+        'https://api.github.com/repos/laurent22/joplin/releases/latest'
     ]
 
     def _parse_assets(self, app_id, version, **data):
@@ -44,25 +50,17 @@ class GithubSpider(scrapy.Spider):
 
 
     def parse(self, response):
-        app_id = response.url.rsplit('/', 2)[-2]
-        json_dict = json.loads(response.text())
-        if app_id == 'git':
-            p = re.compile('^Git\sfor\sWindows.*')
-            tmp = [x for x in json_dict if p.match(x['name'])]
-            filtered_dict = tmp[0]
-            version = filtered_dict['name'].replace('Git for Windows ', '').strip()
-        else:
-            filtered_dict = json_dict[0]
-            version = filtered_dict['tag_name']
-
-        date = filtered_dict['created_at']
+        app_id = response.url.rsplit('/', 3)[-3]
+        json_dict = response.json()
+        version = json_dict['tag_name']
+        date = json_dict['created_at']
 
         item = AppMonitorItem()
         item['name'] = app_id
         item['version'] = version
         item['date'] = date
         item['notes'] = ''
-        item['app_id'] = app_id
-        item['download_url'] = self._parse_assets(app_id, version, filtered_dict)
+        item['id'] = app_id
+        item['download_url'] = json_dict['html_url']
         return item
 
