3acf391372c190fb25d2006b6fb0e330afe56bd3
Siwen Yu <yusiwen@gmail.com>
2020-10-12 18:40:15 +0800

    update fman spider

--- app_monitor/spiders/fman.py
+++ app_monitor/spiders/fman.py
@@ -8,24 +8,14 @@ class FmanSpider(scrapy.Spider):
     allowed_domains = ['fman.io']
     start_urls = ['https://fman.io/download']
 
-    def _parse_changelog(self, response):
-        item = response.meta['item']
-        notes = response.xpath('//h2[@id="v1.6.5"]/following-sibling::*[count(preceding-sibling::h2[@id="v1.6.4"])=0]')
-        notes = notes[:len(notes) - 4]
-        notes = ''.join(notes.getall())
-        item['notes'] = notes
-        yield item
-
     def parse(self, response):
-        version = response.xpath('//div[@class="main-content"]//p[@class="icon-caption"]/text()').get().strip()
-        version = version.split()[1]
-        date = response.xpath('//div[@class="main-content"]//p[@class="icon-caption"]/br/following-sibling::text()').get().strip()
-
+        version = response.xpath('//div[@class="main-content"]//p[contains(text(), "Version")]/text()').get().strip().rsplit(' ')[-1]
 
         item = AppMonitorItem()
         item['name'] = 'fman'
         item['version'] = version
-        item['date'] = date
+        item['date'] = ''
         item['id'] = 'fman'
         item['download_url'] = 'https://fman.io/download'
-        yield scrapy.Request('https://fman.io/changelog', meta={'item':item}, callback=self._parse_changelog)
+        item['notes'] = 'Changelog: https://fman.io/changelog'
+        yield item
