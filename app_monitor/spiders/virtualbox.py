a0f4dbc674afea1cbf64544e491c016aad8d60d1
Siwen Yu <yusiwen@gmail.com>
2020-10-14 19:25:46 +0800

    add crawler for virtualbox

---
+++ app_monitor/spiders/virtualbox.py
@@ -0,0 +1,25 @@
+import scrapy
+from app_monitor.items import AppMonitorItem
+
+
+class VirtualboxSpider(scrapy.Spider):
+    name = 'virtualbox'
+    allowed_domains = ['virtualbox.org']
+    start_urls = ['https://www.virtualbox.org/wiki/Downloads/']
+
+    def parse(self, response):
+        version = response.xpath(
+            '//h3[contains(@id, "platformpackages")]/text()').get().strip().split(' ')[0]
+        item = AppMonitorItem()
+        item['name'] = 'VirtualBox'
+        item['version'] = version
+        item['date'] = ''
+        item['notes'] = ''
+        item['id'] = 'virtualbox'
+
+        urls = []
+        urls.append(response.xpath('//a[@class="ext-link"][contains(text(), "Windows hosts")]/@href').get())
+        urls.append(response.xpath('//a[@class="ext-link"][contains(text(), "OS X hosts")]/@href').get())
+        urls.append('https://www.virtualbox.org/wiki/Linux_Downloads')
+        item['download_url'] = urls
+        return item
