20a540a2912eb1a4bafa3caca1dead1266e87c5b
Siwen Yu <yusiwen@gmail.com>
2020-10-15 14:24:22 +0800

    modularize mail sending procedure

---
+++ app_monitor/mail.py
@@ -0,0 +1,58 @@
+import smtplib
+import ssl
+import logging
+
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from app_monitor import settings
+
+
+def _gen_mail(item):
+    # Create the container (outer) email message.
+    msg = MIMEMultipart('alternative')
+    msg['Subject'] = item['name'] + ' Update Found'
+    text = "{name}\n{version}\n{date}\n{notes}\n{download_url}".format(
+        **item)
+    html = """<html><head></head><body>\
+            <p>{name}</p>
+            <p>{version}</p>
+            <p>{date}</p>
+            <p>{notes}</p>""".format(**item)
+    urls = item['download_url']
+    dwn_str = ''
+    if isinstance(urls, str):
+        dwn_str = "<p><a href='{}'>{}</a></p>".format(
+            urls, urls)
+    elif isinstance(urls, list):
+        for x in urls:
+            dwn_str += "<p><a href='{}'>{}</a></p>".format(
+                x, x)
+    else:
+        dwn_str = ''
+
+    html += dwn_str + '</body></html>'
+
+    msg.attach(MIMEText(text, 'plain'))
+    msg.attach(MIMEText(html, 'html'))
+    return msg
+
+
+def send_mail(item):
+    logging.info('Send mail.....')
+
+    message = _gen_mail(item)
+    message['From'] = settings.SMTP_SENDER
+    message['To'] = settings.SMTP_RECEIVER
+
+    context = ssl.create_default_context()
+    with smtplib.SMTP(settings.SMTP_SERVER, settings.SMTP_PORT) as server:
+        server.ehlo()  # Can be omitted
+        server.starttls(context=context)
+        server.ehlo()  # Can be omitted
+        server.login(settings.SMTP_USERNAME,
+                     settings.SMTP_PASSWORD)
+        logging.debug('Mail server logged in')
+        server.sendmail(settings.SMTP_SENDER,
+                        settings.SMTP_RECEIVER, message.as_string())
+        server.quit()
+    logging.info('Mail sent')
--- app_monitor/pipelines.py
+++ app_monitor/pipelines.py
@@ -1,15 +1,11 @@
 # -*- coding: utf-8 -*-
-import smtplib
-import ssl
 import os
 import errno
 import logging
-import configparser
-import app_monitor.settings
 
 from packaging import version
-from email.mime.multipart import MIMEMultipart
-from email.mime.text import MIMEText
+from app_monitor import settings
+from app_monitor import mail
 
 # Define your item pipelines here
 #
@@ -18,54 +14,6 @@ from email.mime.text import MIMEText
 
 
 class AppMonitorPipeline(object):
-    def _gen_mail(self, item):
-        # Create the container (outer) email message.
-        msg = MIMEMultipart('alternative')
-        msg['Subject'] = item['name'] + ' Update Found'
-        text = "{name}\n{version}\n{date}\n{notes}\n{download_url}".format(
-            **item)
-        html = """<html><head></head><body>\
-                <p>{name}</p>
-                <p>{version}</p>
-                <p>{date}</p>
-                <p>{notes}</p>""".format(**item)
-        urls = item['download_url']
-        dwn_str = ''
-        if isinstance(urls, str):
-            dwn_str = "<p><a href='{}'>{}</a></p>".format(
-                urls, urls)
-        elif isinstance(urls, list):
-            for x in urls:
-                dwn_str += "<p><a href='{}'>{}</a></p>".format(
-                    x, x)
-        else:
-            dwn_str = ''
-
-        html += dwn_str + '</body></html>'
-
-        msg.attach(MIMEText(text, 'plain'))
-        msg.attach(MIMEText(html, 'html'))
-        return msg
-
-    def _send_mail(self, item):
-        logging.info('Send mail.....')
-
-        message = self._gen_mail(item)
-        message['From'] = app_monitor.settings.SMTP_SENDER
-        message['To'] = app_monitor.settings.SMTP_RECEIVER
-
-        context = ssl.create_default_context()
-        with smtplib.SMTP(app_monitor.settings.SMTP_SERVER, app_monitor.settings.SMTP_PORT) as server:
-            server.ehlo()  # Can be omitted
-            server.starttls(context=context)
-            server.ehlo()  # Can be omitted
-            server.login(app_monitor.settings.SMTP_USERNAME,
-                         app_monitor.settings.SMTP_PASSWORD)
-            logging.debug('Mail server logged in')
-            server.sendmail(app_monitor.settings.SMTP_SENDER,
-                            app_monitor.settings.SMTP_RECEIVER, message.as_string())
-            server.quit()
-        logging.info('Mail sent')
 
     def _write_data(self, filename, item):
         if not os.path.exists(os.path.dirname(filename)):
@@ -88,8 +36,8 @@ class AppMonitorPipeline(object):
                 else:
                     logging.info('No Update found, skipping...')
         else:
-            if app_monitor.settings.SEND_MAIL:
-                self._send_mail(item)
+            if settings.SEND_MAIL:
+                mail.send_mail(item)
         self._write_data(filename, item)
 
     def process_item(self, item, spider):
