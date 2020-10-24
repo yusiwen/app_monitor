b1367f9e8530788e71396bdaa3437082b936b466
Siwen Yu <yusiwen@gmail.com>
2020-10-25 04:11:02 +0800

    add Iosevka for monitor

--- app_monitor/spiders/github.py
+++ app_monitor/spiders/github.py
@@ -20,6 +20,7 @@ class GithubSpider(scrapy.Spider):
         'https://api.github.com/repos/Dreamacro/clash/releases/latest',
         'https://api.github.com/repos/Fndroid/clash_for_windows_pkg/releases/latest',
         'https://api.github.com/repos/be5invis/Sarasa-Gothic/releases/latest',
+        'https://api.github.com/repos/be5invis/Iosevka/releases/latest',
         'https://api.github.com/repos/Eugeny/terminus/releases/latest',
         'https://api.github.com/repos/dbeaver/dbeaver/releases/latest',
         'https://api.github.com/repos/microsoft/vscode/releases/latest',
