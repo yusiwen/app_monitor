6a7cc1c8bfaa9f4b7e24d265f3ff5525ba5dc7e3
Siwen Yu <yusiwen@gmail.com>
2020-11-13 04:38:02 +0800

    add more download urls for golang

--- app_monitor/spiders/go.py
+++ app_monitor/spiders/go.py
@@ -1,4 +1,6 @@
 import scrapy
+
+from string import Template
 from app_monitor.items import AppMonitorItem
 
 
@@ -9,7 +11,13 @@ class GoSpider(scrapy.Spider):
 
     def parse(self, response):
         version = response.xpath('//h3[@id="stable"]/following-sibling::div[1]//h2[1]/text()').get().split(' ')[0].replace('go', '')
-        down_url = response.xpath('//h3[@id="stable"]/following-sibling::div[1]//table//a[contains(text(), "linux-amd64")]/@href').get()
+        down_urls = []
+        down_url_prefix = 'https://golang.org'
+        path_templ = Template('//h3[@id="stable"]/following-sibling::div[1]//table//a[contains(text(), "$arch")]/@href')
+        down_urls.append(down_url_prefix + response.xpath(path_templ.substitute(arch='linux-amd64')).get())
+        down_urls.append(down_url_prefix + response.xpath(path_templ.substitute(arch='darwin-amd64')).get())
+        down_urls.append(down_url_prefix + response.xpath(path_templ.substitute(arch='windows-amd64')).get())
+        
 
         item = AppMonitorItem()
         item['name'] = 'Golang'
@@ -17,5 +25,5 @@ class GoSpider(scrapy.Spider):
         item['date'] = None
         item['notes'] = ''
         item['id'] = 'go'
-        item['download_url'] = 'https://golang.org' + down_url
+        item['download_url'] = down_urls
         return item
