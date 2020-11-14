3b5748b3835588a30a57926e090b89250984e42e
Siwen Yu <yusiwen@gmail.com>
2020-11-14 15:10:07 +0800

    add sysinternals to monitor

---
+++ app_monitor/spiders/sysinternals.py
@@ -0,0 +1,35 @@
+import scrapy
+
+from app_monitor.items import AppMonitorItem
+from datetime import datetime
+
+
+class SysinternalsSpider(scrapy.Spider):
+    name = 'sysinternals'
+    allowed_domains = ['microsoft.com']
+    start_urls = [
+        'https://docs.microsoft.com/zh-cn/sysinternals/downloads/tcpview',
+        'https://docs.microsoft.com/zh-cn/sysinternals/downloads/process-explorer',
+        'https://docs.microsoft.com/zh-cn/sysinternals/downloads/procmon',
+        'https://docs.microsoft.com/zh-cn/sysinternals/downloads/vmmap',
+        'https://docs.microsoft.com/zh-cn/sysinternals/downloads/rammap',
+        'https://docs.microsoft.com/zh-cn/sysinternals/downloads/sdelete'
+
+    ]
+
+    def parse(self, response):
+        app_id = response.request.url.rsplit('/', 3)[-1]
+        version = response.xpath('//main/h1')[0].xpath('text()').get().split(' ')[-1]
+        date = response.xpath('//main//p[contains(text(), "Published: ")]/text()').get().split(': ')[-1]
+        date = datetime.strptime(date, '%B %d, %Y')
+        date = datetime.strftime(date, '%Y-%m-%d')
+        down_url = response.xpath('//main//a[@data-linktype="external"]/@href').get()
+
+        item = AppMonitorItem()
+        item['name'] = app_id
+        item['version'] = version
+        item['date'] = date
+        item['notes'] = ''
+        item['id'] = app_id
+        item['download_url'] = down_url
+        return item
