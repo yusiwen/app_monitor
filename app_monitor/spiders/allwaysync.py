92bbbf72fd7f2f269c9025fcf09a40f1f8ee8221
Siwen Yu <yusiwen@gmail.com>
2020-11-15 13:11:06 +0800

    add allwaysync to monitor

---
+++ app_monitor/spiders/allwaysync.py
@@ -0,0 +1,24 @@
+import scrapy
+
+from app_monitor.items import AppMonitorItem
+
+
+class AllwaysyncSpider(scrapy.Spider):
+    name = 'allwaysync'
+    allowed_domains = ['allwaysync.com']
+    start_urls = ['https://allwaysync.com/download']
+
+    def parse(self, response):
+        version = response.xpath('//section//div[@class="panel-body"]/p/strong/text()').get()
+        down_urls = []
+        down_urls.append(response.xpath('//a[@id="desktop-download-primary"]/@href').get())
+        down_urls.append(response.xpath('//a[@id="desktop-download-secondary"]/@href').get())
+
+        item = AppMonitorItem()
+        item['name'] = 'Allway Sync'
+        item['version'] = version
+        item['date'] = None
+        item['notes'] = ''
+        item['id'] = 'allwaysync'
+        item['download_url'] = down_urls
+        return item
