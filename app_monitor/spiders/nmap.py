fb48e13270d91c97c9fe8cbc085ea60f13a1d37a
Siwen Yu <yusiwen@gmail.com>
2021-12-26 21:52:06 +0800

    add nmap

---
+++ app_monitor/spiders/nmap.py
@@ -0,0 +1,25 @@
+import scrapy
+
+from app_monitor.items import AppMonitorItem
+
+
+class NmapSpider(scrapy.Spider):
+    name = 'nmap'
+    allowed_domains = ['nmap.org']
+    start_urls = ['https://nmap.org/download.html']
+
+    def parse(self, response):
+        url = response.xpath('//tr/td//b/u[text() = "stable"]/following-sibling::a[1]/text()').get()
+        version = url.split('-')[1]
+        
+        item = AppMonitorItem()
+        item['name'] = 'nmap'
+        item['version'] = version
+
+        item['date'] = None
+        item['notes'] = None
+        item['id'] = 'nmap'
+        item['download_url'] = url
+        
+        return item
+
