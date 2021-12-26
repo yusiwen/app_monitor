f6d40f350adce88ce974ebc686b4241e002a08b9
Siwen Yu <yusiwen@gmail.com>
2021-12-26 22:21:50 +0800

    add wireshark

---
+++ app_monitor/spiders/wireshark.py
@@ -0,0 +1,25 @@
+import scrapy
+
+from app_monitor.items import AppMonitorItem
+
+
+class WiresharkSpider(scrapy.Spider):
+    name = 'wireshark'
+    allowed_domains = ['www.wireshark.org']
+    start_urls = ['http://www.wireshark.org/']
+
+    def parse(self, response):
+        tmp = response.xpath('//section//a[contains(text(), "Stable Release")]/text()').get()
+        version = tmp.split('(')[1].split(')')[0]
+        
+        item = AppMonitorItem()
+        item['name'] = 'Wireshark'
+        item['version'] = version
+
+        item['date'] = None
+        item['notes'] = None
+        item['id'] = 'wireshark'
+        item['download_url'] = response.xpath(
+            '//section//a[contains(text(), "Stable Release")]/parent::div/following-sibling::div[1]//li').getall()
+        
+        return item
