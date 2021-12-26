86bee21e71ab8367a6acb418d9c3bcc2c8536b92
Siwen Yu <yusiwen@gmail.com>
2021-12-26 19:02:27 +0800

    add ruby

---
+++ app_monitor/spiders/ruby.py
@@ -0,0 +1,24 @@
+import scrapy
+
+from app_monitor.items import AppMonitorItem
+
+
+class RubySpider(scrapy.Spider):
+    name = 'ruby'
+    allowed_domains = ['rubyinstaller.org']
+    start_urls = ['https://rubyinstaller.org/downloads/']
+
+    def parse(self, response):
+        url = response.xpath('//h3[contains(text(), "with Devkit")]/following-sibling::ul/li[1]/a[1]/@href').get()
+        version = url.rsplit('/',2)[1].split('-', 1)[1]
+        
+        item = AppMonitorItem()
+        item['name'] = 'Ruby'
+        item['version'] = version
+
+        item['date'] = None
+        item['notes'] = None
+        item['id'] = 'ruby'
+        item['download_url'] = url
+        
+        return item
