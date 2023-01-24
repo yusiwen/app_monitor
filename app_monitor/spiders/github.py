dddba87b0d7fd9e35010a569bd259ccc0560b2bb
Siwen Yu <yusiwen@gmail.com>
2023-01-24 16:47:00 +0800

    add vagrant

--- app_monitor/spiders/github.py
+++ app_monitor/spiders/github.py
@@ -59,6 +59,7 @@ class GithubSpider(scrapy.Spider):
         {'repo': 'harness/drone', 'category': 'tool', 'tags': ['vps', 'ci/cd', 'devops']},
         {'repo': 'hashicorp/consul', 'category': 'develop', 'tags': ['java', 'service discovery',
                                                                      'configuration management', 'microservice']},
+        {'repo': 'hashicorp/vagrant', 'category': 'tool', 'tags': ['virtualization']},
         {'repo': 'HeidiSQL/HeidiSQL', 'category': 'develop', 'tags': ['database']},
         {'repo': 'henrypp/memreduct', 'category': 'tool', 'tags': ['windows', 'memory']},
         {'repo': 'janeczku/calibre-web', 'category': 'tool', 'tags': ['vps', 'book', 'reading']},
