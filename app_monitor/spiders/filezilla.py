b2e8a1bd3b2934e61768f73556008e5edfc53a5c
Siwen Yu <yusiwen@gmail.com>
2023-12-28 15:31:20 +0800

    fix spider 'filezilla'

--- app_monitor/spiders/filezilla.py
+++ app_monitor/spiders/filezilla.py
@@ -4,23 +4,32 @@ from app_monitor.items import AppMonitorItem
 
 
 class FilezillaSpider(scrapy.Spider):
-    name = 'filezilla'
-    allowed_domains = ['filezilla-project.org']
-    start_urls = ['https://filezilla-project.org/download.php?show_all=1']
+    name = "filezilla"
+    allowed_domains = ["filezilla-project.org"]
+    start_urls = ["https://filezilla-project.org/download.php?show_all=1"]
+    custom_settings = {"USER_AGENT": "app_monitor (+http://es.yusiwen.com)"}
 
     def parse(self, response, **kwargs):
-        version = response.xpath('//p[contains(text(), "latest stable version")]/text()').get().split(' ')[-1]
-        down_urls = [response.xpath('//a[contains(text(), "win64.zip")]/@href').get(),
-                     response.xpath('//a[contains(text(), "macosx-x86.app.tar.bz2")]/@href').get(),
-                     response.xpath('//a[contains(text(), "x86_64-linux-gnu.tar.bz2")]/@href').get()]
+        version = (
+            response.xpath('//p[contains(text(), "latest stable version")]/text()')
+            .get()
+            .split(" ")[-1]
+        )
+        down_urls = [
+            response.xpath('//a[contains(text(), "win64.zip")]/@href').get(),
+            response.xpath('//a[contains(text(), "macos-x86.app")]/@href').get(),
+            response.xpath('//a[contains(text(), "x86_64-linux-gnu.tar")]/@href').get(),
+        ]
 
         item = AppMonitorItem()
-        item['name'] = 'FileZilla'
-        item['version'] = version
-        item['date'] = None
-        item['notes'] = ''
-        item['id'] = 'filezilla'
-        item['category'] = 'tool'
-        item['tags'] = ['ftp', 'sftp']
-        item['download_url'] = down_urls
+        item["name"] = "FileZilla"
+        item["version"] = version
+        item["date"] = None
+        item["notes"] = ""
+        item["id"] = "filezilla"
+        item["category"] = "tool"
+        item["tags"] = ["ftp", "sftp"]
+        item["project_url"] = "https://filezilla-project.org/"
+        item["release_url"] = "https://filezilla-project.org/download.php?show_all=1"
+        item["download_url"] = down_urls
         return item
